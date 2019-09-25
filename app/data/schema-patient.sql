9c27a51a99baef18a43ce926bc5b4d345166bb09
Padric Adams <jopadams@iu.edu>
2019-09-25 09:46:16 -0400

    Notes from class sept 25

--- app/data/schema-patient.sql
+++ app/data/schema-patient.sql
@@ -8,8 +8,23 @@ CREATE TABLE Patient (
     sexAtBirth CHAR(1) DEFAULT ''
 );
 
+CREATE TABLE
+
 INSERT INTO Patient (patientGuid, firstName, lastName, dob, sexAtBirth) VALUES
 ("SOME-REALLY-LONG-1234", "Sylvia", "Hernandez", "2012-09-01",  "F"),
 ("SOME-REALLY-SHORT-5678", "Vish", "Balasubramanian", "1950-12-15",  "M"),
 ("SOME-UNIQUE-ABCDE1", "J", "Doe", "1950-00-00",  ""),
 ("SOME-DUMMY-DATA", "Pepper", "Potts", "1990-01-31",  "F");
+
+CREATE TABLE PatientVisit (
+    visitId INTEGER PRIMARY KEY AUTO_INCREMENT,
+    patientGuid VARCHAR(64) UNIQUE,
+    visitDescription TEXT NOT NULL DEFAULT'',
+    visitDateUtc DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    priority ENUM ('low', 'medium', 'high')
+    FOREIGN KEY (patientGuid) REFERENCES Patient(patientGuid)
+);
+
+
+INSERT INTO PatientVisit (visitId, patientGuid, visitDescription) VALUES
+(1, 'SOME-REALLY-LONG 1234', 'Anxiety from DS');
---
+++ app/public/api/records/index.php
@@ -0,0 +1,26 @@
+<?php
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();
+//:: is a static method
+// Step 2: Create & run the query
+$stmt = $db->prepare('SELECT * FROM Patient'); //the SQL is just a string, prepare gets the SQL ready
+$stmt->execute();  //runs the query
+$patients = $stmt->fetchAll(); //fetching the results of the query, patients is an array, fetchall returns an array of rows
+
+// patientGuid VARCHAR(64) PRIMARY KEY,
+// firstName VARCHAR(64),
+// lastName VARCHAR(64),
+// dob DATE DEFAULT NULL,
+// sexAtBirth CHAR(1) DEFAULT ''
+//just a reminder of schema, non factor
+
+// Step 3: Convert to JSON
+$json = json_encode($patients, JSON_PRETTY_PRINT); // converts array of rows and converts it into json, json_encode is built it, Prettyprint is an
+//option that formats json so easier to read (as humans), all caps means constant
+
+// Step 4: Output
+header('Content-Type: application/json');//php is designed for connections over http, header sends our http header, browser now knows it is json
+//all json files should have that header
+echo $json;
+//end on one blank line, dont close php get_meta_tags
